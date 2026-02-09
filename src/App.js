c1684ae4044ea08d2b5e706761a3e80b99c364a6
Aaron Nolan <YOUR_EMAIL_HEREaaron@benchmarksign.com>
2026-02-09 18:46:29 -0500

    Working Polotno editor

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { PolotnoContainer, SidePanelWrap, WorkspaceWrap } from "polotno";
+import { Toolbar } from "polotno/toolbar/toolbar";
+import { PagesTimeline } from "polotno/pages-timeline";
+import { ZoomButtons } from "polotno/toolbar/zoom-buttons";
+import { SidePanel } from "polotno/side-panel";
+import { Workspace } from "polotno/canvas/workspace";
+
+import "@blueprintjs/core/lib/css/blueprint.css";
+import { createStore } from "polotno/model/store";
+
+const store = createStore({
+  key: "demo",
+  showCredit: true,
+});
+
+store.addPage(); // <-- IMPORTANT: without a page you can get a blank workspace
+
+export default function App() {
+  return (
+    <PolotnoContainer style={{ width: "100vw", height: "100vh" }}>
+      <SidePanelWrap>
+        <SidePanel store={store} />
+      </SidePanelWrap>
+
+      <WorkspaceWrap>
+        <Toolbar store={store} downloadButtonEnabled />
+        <Workspace store={store} />
+        <ZoomButtons store={store} />
+        <PagesTimeline store={store} />
+      </WorkspaceWrap>
+    </PolotnoContainer>
+  );
+}
